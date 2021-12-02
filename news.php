f1533ac1bd02d240702b0e06dcdc0b520ab112b9
Bartholomew Chwalek <bart.chwalek@gmail.com>
2021-12-02 04:38:13 -0500

    added news service

--- news.php
+++ news.php
@@ -1,5 +1,5 @@
 <?php
- 
+ $base = "http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
 // Make sure SimplePie is included. You may need to change this to match the location of autoloader.php
 // For 1.0-1.2:
  
@@ -11,27 +11,37 @@ require_once('./simplepie/autoloader.php');
 $feed = new SimplePie();
  
 // Set which feed to process.
- $feed->set_feed_url('http://rss.cnn.com/rss/cnn_topstories.rss');
+ $feed->set_feed_url('https://globalnews.ca/montreal/feed/');
 // Run SimplePie.
 $feed->init();
+
+$id = $_REQUEST['id'];
  
 // This makes sure that the content is sent to the browser as text/html and the UTF-8 character set (since we didn't change it).
 $feed->handle_content_type();
  
 // Let's begin our XHTML webpage code.  The DOCTYPE is supposed to be the very first thing, so we'll keep it on the same line as the closing-PHP tag.
-?>
 
-<?php
 	/*
 	Here, we'll loop through all of the items in the feed, and $item represents the current item in the loop.
-	*/
-	foreach ($feed->get_items() as $item):
-	?>
- 
-		<div class="item">
-			<h2><a href="<?php echo $item->get_permalink(); ?>"><?php echo $item->get_title(); ?></a></h2>
-			<p><?php echo $item->get_description(); ?></p>
-			<p><small>Posted on <?php echo $item->get_date('j F Y | g:i a'); ?></small></p>
-		</div>
- 
-	<?php endforeach; ?>
+    */
+
+    if(!isset($id)){
+        $xml = new SimpleXMLElement("<?xml version=\"1.0\" encoding=\"utf-8\" ?><CiscoIPPhoneMenu></CiscoIPPhoneMenu>");
+        $cnt = 0;
+        foreach ($feed->get_items() as $item) {
+        $track = $xml->addChild('MenuItem');
+        $track->addChild('name', $item->get_title());
+        $track->addChild('URL', $base . "?id=" . $cnt);
+        $cnt++;
+    }
+    } else {
+        $xml = new SimpleXMLElement("<?xml version=\"1.0\" encoding=\"utf-8\" ?><CiscoIPPhoneText></CiscoIPPhoneText>");
+        $item = $feed->get_item(($id));
+        $xml->addChild('Text', $item->get_description());
+    }
+
+    
+Header('Content-type: text/xml');
+print($xml->asXML());
+?>
